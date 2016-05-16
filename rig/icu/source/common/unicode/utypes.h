d8737ec3daa82f847513267db104c3f17951778e
Robert Bragg <robert@sixbynine.org>
2016-05-16 23:27:40 +0100

    icu: avoid MSVC dependent debug tricks

--- rig/icu/source/common/unicode/utypes.h
+++ rig/icu/source/common/unicode/utypes.h
@@ -399,7 +399,8 @@ typedef double UDate;
  * some Linux/Unix compilers have problems with defining global new/delete.
  * On Windows, it is _MSC_VER>=1200 for MSVC 6.0 and higher.
  */
-#if defined(__cplusplus) && U_DEBUG && U_OVERRIDE_CXX_ALLOCATION && (_MSC_VER>=1200) && !defined(U_STATIC_IMPLEMENTATION) && (defined(U_COMMON_IMPLEMENTATION) || defined(U_I18N_IMPLEMENTATION) || defined(U_IO_IMPLEMENTATION) || defined(U_LAYOUT_IMPLEMENTATION) || defined(U_LAYOUTEX_IMPLEMENTATION))
+#if 0 
+defined(__cplusplus) && U_DEBUG && U_OVERRIDE_CXX_ALLOCATION && (_MSC_VER>=1200) && !defined(U_STATIC_IMPLEMENTATION) && (defined(U_COMMON_IMPLEMENTATION) || defined(U_I18N_IMPLEMENTATION) || defined(U_IO_IMPLEMENTATION) || defined(U_LAYOUT_IMPLEMENTATION) || defined(U_LAYOUTEX_IMPLEMENTATION))
 
 #ifndef U_HIDE_INTERNAL_API
 /**
